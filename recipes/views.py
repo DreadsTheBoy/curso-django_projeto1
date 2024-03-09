6315292d36b972fb39a6d42a8198f3578139e3c2
DreadsTheBoy <allan.contato.goncalves@hotmail.com>
2024-03-09 14:36:39 -0300

    removing unused files

--- recipes/views.py
+++ recipes/views.py
@@ -13,17 +13,3 @@ def home(request: HttpRequest):
         Minha View Home
     """
     return render(request, 'recipes/home.html')
-
-
-def sobre(request: HttpRequest):
-    """
-        Minha View Sobre
-    """
-    return render(request, 'sobre.html')
-
-
-def contato(request: HttpRequest):
-    """
-        Minha View contato
-    """
-    return render(request, 'contato.html')
